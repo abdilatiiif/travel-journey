038f6ef61a02422e96a80b8c1a6cb7a590004e57
Latif <latif_1995@live.no>
2024-11-30 14:26:15 +0100

    starting file ready for production

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="" alt="globe icon" /> // globe icon coming here
+      <h1>my travel journal.</h1>
+    </header>
+  );
+}
